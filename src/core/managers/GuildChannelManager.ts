55dac5b87a552cf9da1b8852152770ba819221bb
devdimer <henrique.mdimer@gmail.com>
2024-06-23 16:28:36 -0300

    fix: invalid channels type

--- src/core/managers/GuildChannelManager.ts
+++ src/core/managers/GuildChannelManager.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Client } from "../Client";
 import { Collection } from "../classes/Collection";
 import { Guild } from "../classes/Guild";
@@ -6,7 +7,7 @@ import { TextChannel } from "../classes/TextChannel";
 import { ChannelTypes } from "../constants/ChannelTypes";
 
 export class GuildChannelManager extends Collection<GuildChannel> {
-  public constructor(client: Client, guild: Guild, channels?: GuildChannel[]) {
+  public constructor(client: Client, guild: Guild, channels?: any[]) {
     super();
     if (!client || !(client instanceof Client)) throw new Error('GuildChannelManager(client): client is missing or invalid');
     if (!guild || !(guild instanceof Guild)) throw new Error('GuildChannelManager(guild): guild is missing or invalid');
