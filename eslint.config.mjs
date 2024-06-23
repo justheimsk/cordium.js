0513aab0eb6061c2347e47e62d783e96bf3a28ab
justheimsk <vonderheimsk@gmail.com>
2024-06-23 11:44:26 -0300

    chore: add eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import tseslint from "typescript-eslint";
 
 
 export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
