e78c71d1c4f2fb3ad32ab3c4559477b05e8af8fc
justheimsk <vonderheimsk@gmail.com>
2024-06-23 16:29:43 -0300

    feat: add users to client cache

---
+++ src/core/managers/UserManager.ts
@@ -0,0 +1,20 @@
+import { Collection } from "../classes/Collection";
+import { User } from "../classes/User";
+import { Client } from "../Client";
+
+export class UserManager extends Collection<User> {
+  public constructor(client: Client, users?: User[]) {
+    super();
+
+    if (!client || !(client instanceof Client)) throw new Error('UserManager(client): client is missing or invalid');
+
+    if (users) {
+      if (!Array.isArray(users)) users = [users];
+
+      for (const _user of users) {
+        const user = new User(client, _user);
+        super.set(user.id, user);
+      }
+    }
+  }
+}
