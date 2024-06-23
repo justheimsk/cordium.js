5a6e1e82d6d42f3da556ce0160dd3e6000cfcc41
justheimsk <vonderheimsk@gmail.com>
2024-06-23 17:02:01 -0300

    test: add eval command for runtime testing

---
+++ src/test/commands/eval.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Client } from "../../core/Client"
+import { Message } from "../../core/classes/Message"
+import util from 'util';
+import { ClusterClient } from "../../core/cluster/ClusterClient";
+
+export default async (client: Client | ClusterClient, msg: Message) => {
+  try {
+    let content: any = msg.content.split(' ');
+    content.shift();
+    content = content.join(' ');
+
+    if (!msg.content || msg.author.id !== '465599065007194113') return await msg.channel?.send('Vai tu te fude');
+    let evalued = eval(`(async () => {${content}})()`);
+
+    if (evalued && evalued.constructor.name == "Promise")
+      evalued = await evalued;
+
+    if (typeof evalued !== "string")
+      evalued = util.inspect(evalued, { depth: 1 });
+
+    evalued = evalued
+      .replace(/`/g, "`" + String.fromCharCode(8203))
+      .replace(/@/g, "@" + String.fromCharCode(8203));
+
+    await msg.channel?.send(`\`\`\`js\n${evalued}\`\`\``)
+  } catch (err) {
+    await msg.channel?.send(`\`\`\`js\n${err}\`\`\``)
+  }
+}
