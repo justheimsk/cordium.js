4ee082278681944fad0c191a1188d7dec585eef8
justheimsk <vonderheimsk@gmail.com>
2024-06-24 17:58:33 -0300

    refactor: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,17 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2]
+    },
+    ignores: ['node_modules/*']
+  }
 ];
