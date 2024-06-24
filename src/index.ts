a01133e1ead33893c39e0e6e8726f5384cfc8c16
justheimsk <vonderheimsk@gmail.com>
2024-06-24 18:00:20 -0300

    chore: export all members from lib

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,36 @@
+// Base
 export * from './core/Client';
 export * from './core/Constants';
+
+// Cache
+export * from './core/cache/ClientCache';
+export * from './core/cache/GuildCache';
+
+// Managers
+export * from './core/managers/UserManager';
+export * from './core/managers/GuildManager';
+export * from './core/managers/GuildMemberManager';
+export * from './core/managers/GuildChannelManager';
+
+// Rest
+export * from './core/rest/RequestManager';
+
+// Gateway
+export * from './core/gateway/GatewayManager';
+export * from './core/gateway/Shard';
+
+// IPC
+export * from './core/cluster/ClusterManager';
+export * from './core/cluster/ClusterClient';
+export * from './core/cluster/IPC';
+
+// Classes
+export * from './core/classes/Collection';
+export * from './core/classes/Guild';
+export * from './core/classes/GuildChannel';
+export * from './core/classes/GuildMember';
+export * from './core/classes/Message';
+export * from './core/classes/User';
+export * from './core/classes/TextChannel';
+export * from './core/classes/ShardPayload';
+export * from './core/classes/IPCMessage';
