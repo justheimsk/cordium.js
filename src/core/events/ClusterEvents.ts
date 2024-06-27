ed22cbaa88a8d048af5fdb125b14cd07e755802b
justheimsk <vonderheimsk@gmail.com>
2024-06-27 18:29:27 -0300

    feat: add workerExit event & add support to communication between 2 workers and not all

--- src/core/events/ClusterEvents.ts
+++ src/core/events/ClusterEvents.ts
@@ -1,6 +1,8 @@
+import { Worker } from 'cluster';
 import { Observable } from '../classes/Observable';
 import { ClusterClient } from '../cluster/ClusterClient';
 
 export class ClusterEvents {
   public workerSpawned = new Observable<ClusterClient>();
+  public workerExit = new Observable<Worker>();
 }
