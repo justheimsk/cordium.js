e5607c3562e4bb0ace0b92cad27899585790e060
justheimsk <vonderheimsk@gmail.com>
2024-06-29 17:18:19 -0300

    docs: add documentation website

---
+++ docs/theme.config.jsx
@@ -0,0 +1,15 @@
+export default {
+  logo: <span>Cordium.js Documentation</span>,
+  project: {
+    link: 'https://github.com/devdimer/cordium.js'
+  },
+  docsRepositoryBase: "https://github.com/devdimer/cordium.js/tree/main/docs",
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Cordium.js Documentation</title>
+      <meta property="og:title" content="Cordium.js Documentation" />
+      <meta property="og:description" content="The documentation for cordium.js library." />
+    </>
+  )
+};
