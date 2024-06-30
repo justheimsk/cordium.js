32844aeef829f540c647fbd0a8ebbf6b084ba852
justheimsk <vonderheimsk@gmail.com>
2024-06-30 14:05:28 -0300

    build improve scripts

--- scripts/organizeDocs.sh
+++ scripts/organizeDocs.sh
@@ -1,3 +1,4 @@
 mv ./docs/pages/Reference/README.mdx ./docs/pages/index.mdx
-echo '{ "index": "Introduction" }' >> ./docs/pages/_meta.json
-echo '{ "classes": "Classes", "interfaces": "Interfaces", "type-aliases": "Type Aliases", "variables": "Variables" }' >> ./docs/pages/Reference/_meta.json
+echo '{ "index": "Introduction" }' > ./docs/pages/_meta.json
+echo '{ "classes": "Classes", "interfaces": "Interfaces", "type-aliases": "Type Aliases", "variables": "Variables" }' > ./docs/pages/Reference/_meta.json
+cp ./scripts/templates/_app.tsx ./docs/pages
