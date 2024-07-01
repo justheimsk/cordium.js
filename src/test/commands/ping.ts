ddfa821da5a02f47f497d4855769f147950c1c80
devdimer <henrique.mdimer@gmail.com>
2024-07-01 16:25:33 -0300

    feat: improve client IPC responses & add method IPCMessage.reply

--- src/test/commands/ping.ts
+++ src/test/commands/ping.ts
@@ -6,16 +6,12 @@ import { ClusterClient } from '../../core/cluster/ClusterClient';
 export default async (client: ClusterClient | Client, msg: Message) => {
   if (client instanceof ClusterClient) {
     const pings = await client.ipc.broadcast('client.shards.ping');
-    let ping = 0;
-
-    for (const Ping of pings.map((r: any) => r.data)) {
-      ping += Ping;
-    }
+    let ping = pings.map((d) => d.content as number).reduce((acc, cur) => acc + cur, 0);
 
     ping = ping / pings.length;
     const shards = await client.ipc.broadcast('client.shards.size');
 
-    await msg.sendReference(`Ping: \`${ping}ms\` (Cluster ID=\`${client.workerId}\`/PID=\`${process.pid}\`)\nClusters: (\`${pings.length}\`)\n${pings.map((c: any) => `--> Cluster (\`${c.workerId}\`): \`${c.data}\`ms (Shards: ${shards.find((r: any) => r.workerId == c.workerId).data})`).join('\n')}`);
+    await msg.sendReference(`Ping: \`${ping}ms\` (Cluster ID=\`${client.workerId}\`/PID=\`${process.pid}\`)\nClusters: (\`${pings.length}\`)\n${pings.map((c: any) => `--> Cluster (\`${c.workerId}\`): \`${c.content}\`ms (Shards: ${shards.find((r: any) => r.workerId == c.workerId)?.content})`).join('\n')}`);
   } else {
     await msg.sendReference(`Ping: \`${client.shards.ping}\`ms (PID = \`${process.pid}\`)\nShards: (\`${client.shards.size}\`)\n${Array.from(client.shards.values()).map((s) => `--> (ID = ${s.id}): \`${s.ping}\`ms`).join('\n')}`);
   }
