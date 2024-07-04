9303ba21609506261a52f407499525f96f2cce0f
devdimer <henrique.mdimer@gmail.com>
2024-07-04 15:17:29 -0300

    feat: add fetch method to fetch data that is not in cache

--- src/core/cache/GuildMemberCache.ts
+++ src/core/cache/GuildMemberCache.ts
@@ -5,11 +5,16 @@ import { Guild } from '../classes/Guild';
 import { GuildMember } from '../classes/GuildMember';
 
 export class GuildMemberCache extends Collection<GuildMember> {
+  #client: Client;
+  public guild: Guild;
+
   public constructor(client: Client, guild: Guild, members?: any[]) {
     super();
     if (!client || !(client instanceof Client)) throw new Error('GuildMemberManager(client): client is missing or invalid.');
     if (!guild || !(guild instanceof Guild)) throw new Error('GuildMemberManager(guild): guild is missing or invalid');
 
+    this.#client = client;
+    this.guild = guild;
     if (members) {
       if (!Array.isArray(members)) members = [members];
 
@@ -19,5 +24,12 @@ export class GuildMemberCache extends Collection<GuildMember> {
         client.cache.users.set(member.user.id, member.user);
       }
     }
+  }
+
+  public async fetch(guildId: string, memberId: string) {
+    if (!guildId) throw new Error('GuildMemberCache.fetch(guildId): guildId is required but is missing.');
+    if (!memberId) throw new Error('GuildMemberCache.fetch(memberId): memberId is required but is missing.');
+
+    return new GuildMember(this.#client, this.guild, await this.#client.rest.request({ method: 'GET', endpoint: `/guilds/${guildId}/${memberId}`, auth: true }));
   }
 }
